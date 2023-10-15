53a7ae529a9b0fa2d64afc9c0e876b7ac9997c75
Christian Olofsson <ch.olofsson@protonmail.com>
2023-10-15 16:41:59 +0200

    Basename added

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename='ProductManagerFrontEnd'>
         <Routes>
           <Route
             path="/"
